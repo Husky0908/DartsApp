a58f35f9257ea4d84a482d6a99dfe4b4369dcb08
husky <zsigmond.tatrai@gmail.com>
2026-01-20 20:47:29 +0100

    menu kezdetlegesen kesz es a main is jo

--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 #include "game.cpp"
 
 int main() {
+    int points=2, players=0, darts_out=5;
+    
     std::cout << "Darts App" << std::endl;
 
-    menu();
-    game();
+    menu(&points, &players, &darts_out);
+    game(points, players, darts_out);
     
     return 0;
 }
--- menu.cpp
+++ menu.cpp
@@ -1,3 +1,17 @@
-void menu() {
-    std::cout << "This is the menu." << std::endl;
+void menu(int *points, int *players, int *darts_out) {
+    
+    while (*points!=0 && *points!=1) {
+        std::cout << "301 or 501 (0, 1)" << std::endl;
+        std::cin >> *points;
+    }
+
+    while (*players<1 || *players>4) {
+        std::cout << "How many players? (2-4): ";
+        std::cin >> *players;
+    }
+
+    while (*darts_out!=0 && *darts_out!=1) {
+        std::cout << "Simple/Double out? (0, 1) " << std::endl;
+        std::cin >> *darts_out;
+    }
 }
