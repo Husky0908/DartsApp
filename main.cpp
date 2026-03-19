9c60aa766166ec4dee0d1a2682f174648adac2b6
GerGoo-ph <grg.kollar@gmail.com>
2026-03-19 20:47:32 +0100

    Semmi.

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 #include "game.cpp"
 
 int main() {
+
     int points, players=2, darts_out=1;
-    
+
     std::cout << "Darts App" << std::endl;
 
     menu(&points, &players, &darts_out);
     game(points, players, darts_out);
-    
+
     return 0;
 }
